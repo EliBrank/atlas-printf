29485c0f55d27e765b4333279a118757354cc9d8
root <kelciatkinson@gmail.com>
2024-02-28 18:41:54 +0000

    added _printf file with skeleton outline and prototype to header file

---
+++ _printf.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf- prints string with format specifiers
+ *
+ * @format: string containing the format specifier(s)
+ *
+ * Description: writes output based on a format string that
+ * specifies how subsequent arguments are converted for output
+ *
+ * Return: length of printed string
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
